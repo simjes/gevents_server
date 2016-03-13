bb3cc83e14c7cc4ae6f481e98f1fac05d5720d2d
Simon Jespersen <s.jespersen@stud.uis.no>
2016-03-13 16:11:15 +0100

    local events done

--- routes/api.js
+++ routes/api.js
@@ -3,19 +3,22 @@ var router = express.Router();
 var db = require('../lib/db_com');
 
 router.route('/events')
-    .get(function (req, res) {
-        var type = req.query.type;
-        if (type != null) {
-            db.getEventsByType(type, function (err, events) {
+    .get(function(req, res) {
+        if (req.query.type != null) {
+            db.getEventsByType(req.query.type, function(err, events) {
+                res.send(events);
+            });
+        } else if (req.query.lng != null && req.query.lat != null) {
+            db.getLocalEvents({ lng: req.query.lng, lat: req.query.lat }, function(err, events) {
                 res.send(events);
             });
         } else {
-            db.getAllEvents(function (err, events) {
+            db.getAllEvents(function(err, events) {
                 res.send(events);
             });
         }
     })
-    .post(function (req, res) { //TODO: check if event allready exists
+    .post(function(req, res) { //TODO: check if event allready exists
         if (eventExists(req.body)) {
             res.send({ lel: "alrdy exists" });
         } else {
@@ -30,12 +33,12 @@ function eventExists(event) {
 }
 
 function saveEvent(event, apiResponse) {
-    handleHosts(event.hosts); 
+    handleHosts(event.hosts);
     //Check if user is approved to add events. Event is approved if the user is approved.
-    db.getUser(event.uploader.fb_id, function (err, user) {
+    db.getUser(event.uploader.fb_id, function(err, user) {
         if (user === null) {
             event.uploader.approved = false;
-            db.addUser(event.uploader, function () {
+            db.addUser(event.uploader, function() {
                 event.approved = false;
             });
         } else {
@@ -50,8 +53,8 @@ function saveEvent(event, apiResponse) {
 }
 
 function handleHosts(hosts) {
-    hosts.forEach(function (host) {
-        db.getHostInfo(host.name, function (err, hostInfo) {
+    hosts.forEach(function(host) {
+        db.getHostInfo(host.name, function(err, hostInfo) {
             if (hostInfo === null) {
                 db.addHost(host);
             }
@@ -60,7 +63,7 @@ function handleHosts(hosts) {
 }
 
 function addEventAndRespond(eventInfo, apiResponse) {
-    db.addEvent(eventInfo, function (err, result) {
+    db.addEvent(eventInfo, function(err, result) {
         console.log(result);
         apiResponse.send(result);
     });
@@ -69,8 +72,8 @@ function addEventAndRespond(eventInfo, apiResponse) {
 
 //Get full details for an event.
 router.route('/events/:event_id')
-    .get(function (req, res) {
-        db.getEvent(req.params.event_id, function (err, event) {
+    .get(function(req, res) {
+        db.getEvent(req.params.event_id, function(err, event) {
             res.send(event);
         });
     });
